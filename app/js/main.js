86d56f9dc218b4efd6d0107aa89d93192a905204
jk <jkpas30@gmail.com>
2016-08-23 21:20:24 +0300

    index.html

--- app/js/main.js
+++ app/js/main.js
@@ -7,8 +7,47 @@ $(document).ready(function(){
 
 	$('.bxslider2').bxSlider({
   		pager: false,
-  		infiniteLoop: true
+  		infiniteLoop: true,
+  		displaySlideQty: 6
   	});
+
+	$(".header_nav>ul>li>a,.hover_menu").mouseover(function() {
+		$(".hover_menu").css( "display", "block" )		
+		})
+		.mouseout(function() {
+	    $(".hover_menu").css( "display", "none" )
+	});
+	
+	// $(".header_nav>ul>item1>a").mouseover(function() {
+	// 	$(".header_nav>ul>item1>span").css( "display", "block" )		
+	// 	})
+	// 	.mouseout(function() {
+	//     $(".header_nav>ul>item1>span").css( "display", "none" )
+	// });
+	// $(".header_nav>ul>item2>a").mouseover(function() {
+	// 	$(".header_nav>ul>item2>span").css( "display", "block" )		
+	// 	})
+	// 	.mouseout(function() {
+	//     $(".header_nav>ul>item2>span").css( "display", "none" )
+	// });
+	// $(".header_nav>ul>item3>a").mouseover(function() {
+	// 	$(".header_nav>ul>item3>span").css( "display", "block" )		
+	// 	})
+	// 	.mouseout(function() {
+	//     $(".header_nav>ul>item3>span").css( "display", "none" )
+	// });
+	// $(".header_nav>ul>item4>a").mouseover(function() {
+	// 	$(".header_nav>ul>item4>span").css( "display", "block" )		
+	// 	})
+	// 	.mouseout(function() {
+	//     $(".header_nav>ul>item4>span").css( "display", "none" )
+	// });
+	// $(".header_nav>ul>item5>a").mouseover(function() {
+	// 	$(".header_nav>ul>item5>span").css( "display", "block" )		
+	// 	})
+	// 	.mouseout(function() {
+	//     $(".header_nav>ul>item5>span").css( "display", "none" )
+	// });	
 
 });
 
